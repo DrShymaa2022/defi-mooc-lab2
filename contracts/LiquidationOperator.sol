e364a67a867dfaf39782e6655486e03537023d93
DrShymaa2022 <105880363+DrShymaa2022@users.noreply.github.com>
2022-05-27 15:28:51 +0200

    Update LiquidationOperator.sol

--- contracts/LiquidationOperator.sol
+++ contracts/LiquidationOperator.sol
@@ -268,7 +268,7 @@ interface IUniswapV2Pair {
            bytes memory data= "any non null string"; //for the call to be flashloan not regular swap
            // uniswapV2Call(usdt_wbtc_pair,(0.6*totalDebtETH), 0, data);
            //should get the flashloan value in btc like the value locked, I think?
-           uint256 value =0.7*totalDebtETH*3000000;
+           uint256 value =totalDebtETH*2300000;
            console.log("flashloan value= %d",value);
            IUniswapV2Pair(usdt_wbtc_pair).swap(value, 0, address(this), data);
         //I'm taking a Flashloan that is roughly larger than both liquidation steps, since no harm is done if it is larger
