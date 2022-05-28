1a6371aee639663a2e5400b8f79611dcab5f5811
DrShymaa2022 <105880363+DrShymaa2022@users.noreply.github.com>
2022-05-28 12:02:35 +0200

    Update LiquidationOperator.sol
    no such const as maxuint defined, hope -1 doesn't turn into simply 1!

--- contracts/LiquidationOperator.sol
+++ contracts/LiquidationOperator.sol
@@ -320,7 +320,7 @@ interface IUniswapV2Pair {
         // 2.1 liquidate the target user
         //    *** Your code here ***
         uint256 paidvalue = -1;
-        paidvalue = MAX_UINT;
+        console.log("paidvalue =",paidvalue);
         lendingPool.liquidationCall(token0, token1, address(this), paidvalue , false); //changed from Aave to -1 limit 
         
         // 2.2 swap WBTC for other things or repay directly
