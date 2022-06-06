6440421e67bf4fe214b22022b403d9a8c1824c4e
DrShymaa2022 <105880363+DrShymaa2022@users.noreply.github.com>
2022-06-06 13:25:33 +0200

    Update LiquidationOperator.sol
    1-the liquidated quantity should be subtracted only in the last step, that's why there was an error leading to a negative number
    2-adding the 1 to 1901 in the first repay lead to less liquidated value in total so back to 190111 to end up with 9609 colletral with 7165 debt and this gain 94

--- contracts/LiquidationOperator.sol
+++ contracts/LiquidationOperator.sol
@@ -261,7 +261,7 @@ contract LiquidationOperator is IUniswapV2Callee {
         
 
         // Fine-tuned value. Should be greater than closing factor, but not too much...
-        uint256 debtToCoverUSDT = 1790000000000;
+        uint256 debtToCoverUSDT = 1770000000000;
 
         // 2. call flash swap to liquidate the target user
         // based on https://etherscan.io/tx/0xac7df37a43fab1b130318bbb761861b8357650db2e2c6493b73d6da3d9581077
@@ -296,7 +296,7 @@ contract LiquidationOperator is IUniswapV2Callee {
         uint256 amount1,
         bytes calldata
     ) external override {
-        uint112 repay1=191111111111;
+        uint112 repay1=190111111111;
        
        // these 3 lines I need when I comment the 2 liquidation steps part and get back to 1 step
        /* (uint112 w_btc, uint112 w_eth, ) = IUniswapV2Pair(msg.sender)
@@ -333,8 +333,8 @@ contract LiquidationOperator is IUniswapV2Callee {
         console.log("Liquidation Threshold = %d", LqThrshld);
         console.log("LTV= ", ltv);
         
-        collateral_diff=collateral_diff-Collateral_ETH;
-        debt_diff=debt_diff-Debt_ETH;
+        //collateral_diff=collateral_diff-Collateral_ETH;
+        //debt_diff=debt_diff-Debt_ETH;
         }        
                 //2nd liquidation
        
